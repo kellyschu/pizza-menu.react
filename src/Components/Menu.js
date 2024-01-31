a03b82d4d6f0e221435680567f0253984f5918ea
Kelly Schumacher <kschumacher93@gmail.com>
2024-01-31 16:34:16 -0500

    conditionally rendering pizzas with && operator

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,14 +2,19 @@ import Pizza from "../Components/Pizza";
 import pizzaData from "../data";
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <div className="menu">
       <h2> Our Menu </h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
