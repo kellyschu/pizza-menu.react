50314efe7deda5222e4641f1ee80835ef998c559
Kelly Schumacher <kschumacher93@gmail.com>
2024-01-31 16:47:46 -0500

    added soldout return null to pizzas

--- src/Components/Pizza.js
+++ src/Components/Pizza.js
@@ -2,6 +2,7 @@
 // import focaccia from "../pizzas/focaccia.jpg";
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
