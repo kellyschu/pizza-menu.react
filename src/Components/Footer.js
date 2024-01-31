1fa3321299a245fea388a1d3b4fc9585202797a1
Kelly Schumacher <kschumacher93@gmail.com>
2024-01-31 11:49:16 -0500

    boolean value for isOpen to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,9 @@
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
   return (
     <footer>{new Date().toLocaleTimeString()} We are currently open! </footer>
   );
